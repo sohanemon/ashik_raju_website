095d174336e7fe14e2eb7621501c3ffd9cc93856
sohanemon <m.sohanemon@gmail.com>
2023-03-10 18:17:42 +0600

    s:about addded

---
+++ backend/schemas/about.ts
@@ -0,0 +1,38 @@
+import {defineType, defineField} from 'sanity'
+
+export default defineType({
+  name: 'about',
+  title: 'About',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'username',
+      title: 'Username',
+      type: 'string',
+    }),
+    defineField({
+      name: 'about',
+      title: 'About',
+      type: 'text',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+})
--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,6 +1,3 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import about from './about'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [about]
