dcedb6420dc34f3d6ed17ff27864486c31754448
sohanemon <m.sohanemon@gmail.com>
2023-03-11 20:20:05 +0600

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        alpha: '#D6EFED',
+        beta: '#B7D3DF',
+        gamma: '#C9BBCF',
+        psi: '#898AA6',
+        omega: '#898AA6',
+      },
+    },
   },
   plugins: [],
 };
