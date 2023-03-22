1517d95a7d26be24b699501eab3a351b9b2177fa
sohanemon <m.sohanemon@gmail.com>
2023-03-22 11:28:36 +0600

    button condition added

--- components/button.tsx
+++ components/button.tsx
@@ -9,7 +9,9 @@ export default function Button({
     <>
       <button
         className={`${
-          fill ? 'bg-alpha text-psi' : 'bg-psi text-alpha border border-alpha'
+          fill
+            ? 'bg-alpha text-psi hover:bg-psi hover:text-alpha hover:border border-alpha'
+            : 'bg-psi text-alpha border border-alpha hover:bg-alpha hover:text-psi hover:border-none'
         } rounded-md px-5 py-3`}
       >
         {children}
