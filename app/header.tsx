cbc3bfcffc14b053a3b1703fe4b6b68dd8bd4afa
sohanemon <m.sohanemon@gmail.com>
2023-03-22 10:26:01 +0600

    header style updated

--- app/header.tsx
+++ app/header.tsx
@@ -4,9 +4,12 @@ import Title from '@/components/title';
 export default function Header() {
   return (
     <section>
-      <BoldText>Hi there</BoldText>
-      <Title>Ashik Raju</Title>
-      <p className='text-center '>Marketing Expert</p>
+      <BoldText>Hi, its</BoldText>
+      <Title>
+        <h1 className='text-4xl text-white'>Ashik Raju</h1>
+      </Title>
+
+      <p className='text-center text-gray-400 text-sm'>Marketing Expert</p>
     </section>
   );
 }
