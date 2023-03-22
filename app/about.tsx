90367c2947f9ee1568168edd3067295d7f222777
sohanemon <m.sohanemon@gmail.com>
2023-03-22 17:01:05 +0600

    new page added

---
+++ app/about.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <>
+      <div className='bg-red-300 min-h-screen'></div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from './about';
 import Header from './header';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <About />
     </main>
   );
 }
