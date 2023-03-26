42a6a1bc16f7ef5d09bf658d6833e804d8738eaa
sohanemon <m.sohanemon@gmail.com>
2023-03-26 14:03:36 +0600

    skills added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from './about';
 import Header from './header';
+import Skills from './skills';
 
 export default function Home() {
   return (
     <main>
       <Header />
       <About />
+      <Skills />
     </main>
   );
 }
