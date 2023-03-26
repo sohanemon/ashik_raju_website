3779550e0c6b4f26d65173d03ba7299c413d2b70
sohanemon <m.sohanemon@gmail.com>
2023-03-26 14:13:12 +0600

    skillset type added

--- global.ts
+++ global.ts
@@ -1,3 +1,18 @@
 export type Child = {
   children: React.ReactNode | string;
 };
+export type Details = {
+  icon: React.ReactNode;
+  title: string;
+  text: string;
+};
+
+export type Skill = {
+  name: string;
+  level: 'Experienced' | 'Intermediate' | 'Basic';
+};
+
+export type SkillSet = {
+  title: string;
+  skills: Skill[];
+};
