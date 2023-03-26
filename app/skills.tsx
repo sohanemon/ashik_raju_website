a5adf52bbe55ede77efe6cdb9331e7aa839457f0
sohanemon <m.sohanemon@gmail.com>
2023-03-26 14:31:17 +0600

    sub section added

--- app/skills.tsx
+++ app/skills.tsx
@@ -1,6 +1,7 @@
 import BoldText from '@/components/bold-text';
 import Title from '@/components/title';
 import { SkillSet } from '@/global';
+import { SiBlueprint } from 'react-icons/si';
 
 export default function Skills() {
   return (
@@ -12,7 +13,31 @@ export default function Skills() {
           <p className='text-4xl text-white mt-2 mb-1'>My skills</p>
         </Title>
 
-        <section>{/* #note: skills container */}</section>
+        <section>
+          {/* #note: skills container */}
+          <div className='grid grid-cols-2 w-4/5 mx-auto gap-10'>
+            {skills?.map((e) => (
+              <div key={e.title} className='bg-omega p-7  rounded-3xl'>
+                <h1 className='text-xl text-psi text-center mb-4 '>
+                  {e.title}
+                </h1>
+                <div className='space-y-4'>
+                  {e.skills?.map((e) => (
+                    <div key={e.name}>
+                      <div className='flex items-center gap-3 tracking-wider'>
+                        <SiBlueprint className='text-psi' />
+                        <span className='text-psi capitalize font-semibold'>
+                          {e.name}
+                        </span>
+                      </div>
+                      <p className='pl-7 text-beta text-sm'>{e.level}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
     </>
   );
