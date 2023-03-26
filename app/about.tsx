054ed158bb2ef7f6f817cf927dd16f6cc0a8c071
sohanemon <m.sohanemon@gmail.com>
2023-03-26 13:55:14 +0600

    about furnished

--- app/about.tsx
+++ app/about.tsx
@@ -13,45 +13,48 @@ import Button from '@/components/button';
 export default function About() {
   return (
     <>
-      <section className='my-10 min-h-screen'>
-        <BoldText>Get to know</BoldText>
+      <div className='grid place-items-center min-h-screen'>
+        <section className='my-10 min-h-screen'>
+          <BoldText>Get to know</BoldText>
 
-        <Title>
-          <p className='text-4xl text-white mt-2 mb-1'>About me</p>
-        </Title>
+          <Title>
+            <p className='text-4xl text-white mt-2 mb-1'>About me</p>
+          </Title>
 
-        <div className='grid grid-cols-2 w-4/5 mx-auto'>
-          <aside
-            id='image-holder'
-            style={{ backgroundImage: 'url("/images/raju_main.png")' }}
-            className='rounded-xl px-6 pt-10 w-96 h-96 bg-cover after:bg-omega after:rounded-xl'
-          >
-            <div className='absolute bg-gradient-to-tr to-alpha from-alpha inset-0 -z-10 rounded-xl -rotate-12' />
-          </aside>
-          {/* #TODO: details section */}
-          <aside className='grid grid-cols-3 gap-6 '>
-            {details?.map((e) => (
-              <div
-                key={e.title}
-                className='flex flex-col items-center bg-alpha h-max p-5 rounded-lg shadow shadow-omega'
-              >
-                {e.icon}
-                <p className='text-psi font-semibold text-lg'>{e.title}</p>
-                <p className='text-omega text-xs'>{e.text}</p>
+          <div className='grid grid-cols-2 w-4/5 mx-auto place-items-center mt-10'>
+            <aside
+              id='image-holder'
+              style={{ backgroundImage: 'url("/images/raju_main.png")' }}
+              className='rounded-xl px-6 pt-10 w-60 h-72 bg-cover after:bg-omega after:rounded-xl'
+            >
+              <div className='absolute bg-gradient-to-tr to-alpha from-alpha inset-0 -z-10 rounded-xl -rotate-12' />
+            </aside>
+            {/* #TODO: details section */}
+            <aside className='grid grid-cols-3 gap-6 h-max'>
+              {details?.map((e) => (
+                <div
+                  key={e.title}
+                  className='flex flex-col items-center bg-alpha h-max p-5 rounded-lg shadow shadow-omega h-full'
+                >
+                  {e.icon}
+                  <p className='text-psi font-semibold text-lg'>{e.title}</p>
+                  <p className='text-omega text-xs'>{e.text}</p>
+                </div>
+              ))}
+              <div className='col-span-full'>
+                <p className='text-alpha mb-6'>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
+                  obcaecati neque consectetur, corrupti earum voluptas officiis
+                  ullam voluptatibus dolorem odit placeat nulla sapiente
+                  assumenda sit exercitationem, molestias aspernatur impedit
+                  non.
+                </p>
+                <Button fill>Lets connect</Button>
               </div>
-            ))}
-            <div className='col-span-full'>
-              <p className='text-alpha'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
-                obcaecati neque consectetur, corrupti earum voluptas officiis
-                ullam voluptatibus dolorem odit placeat nulla sapiente assumenda
-                sit exercitationem, molestias aspernatur impedit non.
-              </p>
-              <Button fill>Lets connect</Button>
-            </div>
-          </aside>{' '}
-        </div>
-      </section>
+            </aside>{' '}
+          </div>
+        </section>
+      </div>
 
       <style jsx>
         {`
