16b79a85f50ed59a1e851790c98baba08bb0561e
sohanemon <m.sohanemon@gmail.com>
2023-03-26 14:03:30 +0600

    title added

---
+++ app/skills.tsx
@@ -0,0 +1,16 @@
+import BoldText from '@/components/bold-text';
+import Title from '@/components/title';
+
+export default function Skills() {
+  return (
+    <>
+      <div className='min-h-screen'>
+        <BoldText>What skills I have</BoldText>
+
+        <Title>
+          <p className='text-4xl text-white mt-2 mb-1'>My skills</p>
+        </Title>
+      </div>
+    </>
+  );
+}
