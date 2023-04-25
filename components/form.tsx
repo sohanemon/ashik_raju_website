38ed703287c39d904d4b84ed86ccd975ed9ae6e4
sohanemon <m.sohanemon@gmail.com>
2023-04-25 19:26:01 +0600

    form update

--- components/form.tsx
+++ components/form.tsx
@@ -2,30 +2,28 @@ import UiBtn from './ui/ui-btn';
 
 export default function Form() {
   return (
-    <section className='w-full'>
+    <section className='w-full '>
       <div className='relative '>
         <div className='relative  text-gray-500 '>
           <div className='m-auto space-y-8 '>
             <div className='rounded-xl border border-gray-100 bg-alpha shadow-2xl shadow-gray-600/10 backdrop-blur-2xl'>
               <div className='p-8 py-12 sm:p-16'>
-                <form action='' className='space-y-8'>
+                <form action='' className='space-y-3'>
                   <div className='space-y-2'>
-                    <label
-                      htmlFor='email'
-                      className='text-gray-600 dark:text-gray-300'
-                    >
+                    <label className='' htmlFor='email'>
                       Email
                     </label>
                     <input
                       type='email'
                       name='email'
                       id='email'
-                      autoComplete='username'
-                      className='focus:outline-none block w-full rounded-md border border-primary dark:border-gray-600 bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-primary'
+                      placeholder='mail@your-domain.com'
+                      autoComplete='email'
+                      className='focus:outline-none block w-full rounded-md border border-omega  bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-omega'
                     />
                   </div>
 
-                  <div>
+                  <div className='space-y-2'>
                     <label
                       htmlFor='password'
                       className='text-gray-600 dark:text-gray-300'
@@ -37,9 +35,11 @@ export default function Form() {
                       name='pwd'
                       id='pwd'
                       autoComplete='current-password'
-                      className='focus:outline-none block w-full rounded-md border border-gray-200 dark:border-gray-600 bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300'
+                      placeholder='mail@your-domain.com'
+                      className='focus:outline-none block w-full rounded-md border border-omega  bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-omega'
                     />
                   </div>
+                  <br />
                   <UiBtn />
                 </form>
               </div>
